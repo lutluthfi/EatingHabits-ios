39295dda2483ca497ab40d697dcb69a772686706
Faruuq <faruuq.q@icloud.com>
2020-04-09 22:02:26 +0800

    Simplify the code

--- EatingHabits/Presentation/Eat Time Recommendation/EatTimeRecommendationVCViewController.swift
+++ EatingHabits/Presentation/Eat Time Recommendation/EatTimeRecommendationVCViewController.swift
@@ -13,29 +13,21 @@ class EatTimeRecommendationVCViewController: UIViewController {
     @IBOutlet weak var bodyLabel: UILabel!
     @IBOutlet weak var titleLable: UILabel!
     @IBOutlet weak var imageBanner: UIImageView!
+    @IBOutlet weak var closeBtnLabel: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.imageBanner.image = #imageLiteral(resourceName: "EatTime")
+        self.imageBanner.contentMode = .scaleAspectFill
         self.navigationController!.navigationBar.titleTextAttributes = [.foregroundColor: UIColor(red: 7.0/255.0, green: 169.0/255.0, blue: 20.0/255.0, alpha: 1.0)]
+        self.closeBtnLabel.tintColor = UIColor(red: 155.0/255.0, green: 100.0/255.0, blue: 85.0/255.0, alpha: 1.0)
+        self.titleLable.text = "When is the best time to have a meal?"
+        self.bodyLabel.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc felis sapien, ornare vitae orci nec, ultricies egestas turpis. Quisque et consectetur ex. Pellentesque auctor efficitur arcu. Cras vulputate lacus in suscipit fermentum. Proin quis lobortis eros, consectetur scelerisque tellus. Nam mi ex, egestas vel ex et, vehicula vestibulum nisi. Vestibulum semper eros a ipsum congue ultricies. Nullam quis fermentum orci. Duis dapibus, ipsum eget fermentum pulvinar, ante quam efficitur nunc, in aliquet mi nisi sed sapien. Fusce euismod rutrum erat, sit amet aliquet elit laoreet ut.\n\nCras diam velit, sagittis in efficitur sed, venenatis et lorem. Aenean maximus vestibulum eros sit amet semper. Vestibulum et scelerisque purus, vitae suscipit odio. Aenean efficitur quam vel massa lobortis, id tincidunt justo rhoncus. Praesent id viverra libero. Vivamus gravida mi non lacus pellentesque porttitor. In convallis metus leo, in congue lorem euismod laoreet. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque tincidunt ultrices ante, ut porttitor lacus ornare non. Morbi interdum lacus in felis ullamcorper laoreet. In sed consectetur lacus. Proin iaculis odio ac augue lobortis, eget molestie sapien feugiat. Sed felis eros, condimentum eu ornare sed, imperdiet et velit."
     }
     
-    @IBAction func CloseBtn(_ sender: UIBarButtonItem) {
-        self.dismiss(animated: true)
+    @IBAction func closeBtn(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
     }
-    
-    
-    
-    
-    
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
