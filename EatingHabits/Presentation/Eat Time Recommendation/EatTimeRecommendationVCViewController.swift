d826adf560ec14edc36b295eac9950430077f198
Faruuq <faruuq.q@icloud.com>
2020-04-09 17:03:02 +0800

    Create storyboard and its view controller (empty intialization)

---
+++ EatingHabits/Presentation/Eat Time Recommendation/EatTimeRecommendationVCViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EatTimeRecommendationVCViewController.swift
+//  EatingHabits
+//
+//  Created by Muhammad Faruuq Qayyum on 09/04/20.
+//  Copyright © 2020 Triple T. All rights reserved.
+//
+
+import UIKit
+
+class EatTimeRecommendationVCViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
