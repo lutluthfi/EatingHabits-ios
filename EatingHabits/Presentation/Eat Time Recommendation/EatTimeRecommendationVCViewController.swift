2c09f334c32f14e166e76e75948c39868960d436
Faruuq <faruuq.q@icloud.com>
2020-04-09 19:00:41 +0800

    Set info.plist and modal view, embed in navigation controller, add image and labels

--- EatingHabits/Presentation/Eat Time Recommendation/EatTimeRecommendationVCViewController.swift
+++ EatingHabits/Presentation/Eat Time Recommendation/EatTimeRecommendationVCViewController.swift
@@ -9,14 +9,25 @@
 import UIKit
 
 class EatTimeRecommendationVCViewController: UIViewController {
-
+    
+    @IBOutlet weak var bodyLabel: UILabel!
+    @IBOutlet weak var titleLable: UILabel!
+    @IBOutlet weak var imageBanner: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.navigationController!.navigationBar.titleTextAttributes = [.foregroundColor: UIColor(red: 7.0/255.0, green: 169.0/255.0, blue: 20.0/255.0, alpha: 1.0)]
     }
     
-
+    @IBAction func CloseBtn(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true)
+    }
+    
+    
+    
+    
+    
     /*
     // MARK: - Navigation
 
