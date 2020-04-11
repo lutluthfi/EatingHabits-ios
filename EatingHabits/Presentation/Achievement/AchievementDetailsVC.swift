7870c27226cef8421fa2e48b8c2c609f0d242d68
Faruuq <faruuq.q@icloud.com>
2020-04-11 19:21:00 +0800

    Create custom radial gradient view class.

---
+++ EatingHabits/Presentation/Achievement/AchievementDetailsVC.swift
@@ -0,0 +1,42 @@
+//
+//  AchievementDetailsVC.swift
+//  EatingHabits
+//
+//  Created by Muhammad Faruuq Qayyum on 11/04/20.
+//  Copyright © 2020 Triple T. All rights reserved.
+//
+
+import UIKit
+
+class AchievementDetailsVC: UIViewController {
+    
+    @IBOutlet weak var headerLabel: UILabel!
+    @IBOutlet weak var achievementImage: UIImageView!
+    @IBOutlet weak var footerLabel: UILabel!
+    @IBOutlet weak var achievementDescription: UILabel!
+    @IBOutlet weak var boardBtn: UIButton!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        boardBtn.setTitle("Achievement Board", for: .normal)
+        boardBtn.setTitleColor(.white, for: .normal)
+        boardBtn.backgroundColor = UIColor(red: 0/255.0, green: 0/255.0, blue: 0/255.0, alpha: 0.3)
+        boardBtn.layer.cornerRadius = 8
+        
+        view.backgroundColor = .brown
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
