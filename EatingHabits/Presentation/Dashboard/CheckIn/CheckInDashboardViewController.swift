212e7973a7794494808b5318a6dfb11ed1652437
Arif Luthfiansyah <lutluthfi@icloud.com>
2020-04-12 20:07:27 +0700

    Add function to start TimeScheduler to LiveTime in CheckInViewController

--- EatingHabits/Presentation/Dashboard/CheckIn/CheckInDashboardViewController.swift
+++ EatingHabits/Presentation/Dashboard/CheckIn/CheckInDashboardViewController.swift
@@ -11,14 +11,38 @@ import UIKit
 class CheckInDashboardViewController: UIViewController {
 
     @IBOutlet weak var checkInButton: UIButton!
+    @IBOutlet weak var currentTimeLabel: UILabel!
     @IBOutlet weak var currentTimeVisualEffectView: UIVisualEffectView!
     @IBOutlet weak var mealTimeBackgroundImageView: UIImageView!
+    @IBOutlet weak var quotesLabel: UILabel!
     
+    lazy var dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "hh:mm a"
+        dateFormatter.amSymbol = "AM"
+        dateFormatter.pmSymbol = "PM"
+        return dateFormatter
+    }()
     
+    private var timer: Timer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setupViewDidLoad()
+        self.startTimerScheduler()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.timer?.invalidate()
+    }
+    
+    @IBAction func onCheckInButtonTouchUpInside(_ sender: UIButton) {
+        self.showSuccessCheckIn()
+    }
+    
+    @IBAction func onSeeRecommendationTimeContainerViewTapped(_ sender: UITapGestureRecognizer) {
+        
     }
     
     private func setupViewDidLoad() {
@@ -28,5 +52,23 @@ class CheckInDashboardViewController: UIViewController {
         self.currentTimeVisualEffectView.clipsToBounds = true
         self.currentTimeVisualEffectView.layer.cornerRadius = 8
     }
-
+    
+    private func showSuccessCheckIn() {
+        let okayAlertAction = UIAlertAction(title: "OK", style: .default) { (_) in }
+        let alertViewController = UIAlertController(title: "Success", message: "Don't forget to catch your next meal", preferredStyle: .alert)
+        alertViewController.addAction(okayAlertAction)
+        self.present(alertViewController, animated: true) { }
+    }
+    
+    private func startTimerScheduler() {
+        self.timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (_) in
+            self.onUpdateCurrentTimeLabel()
+        }
+    }
+    
+    private func onUpdateCurrentTimeLabel() {
+        self.currentTimeLabel.text = self.dateFormatter.string(from: Date())
+    }
+    
 }
+
