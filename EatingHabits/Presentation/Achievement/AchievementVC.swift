0c8f72eb487e68c8d2a3c620b97caed9a40cf275
Faruuq <faruuq.q@icloud.com>
2020-04-13 08:04:39 +0800

    Fix badge item to dynamic

--- EatingHabits/Presentation/Achievement/AchievementVC.swift
+++ EatingHabits/Presentation/Achievement/AchievementVC.swift
@@ -15,12 +15,11 @@ class AchievementVC: UIViewController {
     var selectedImage: UIImage!
     
     let section = [
-        SectionHeader(section: "Silver", badgeName: "imagePlaceholder"),
-        SectionHeader(section: "Gold", badgeName: "LockedAchievement"),
-        SectionHeader(section: "Onyx", badgeName: "LockedAchievement"),
-        SectionHeader(section: "Ruby", badgeName: "LockedAchievement")
+        SectionHeader(section: "Silver", badgeName: ["silver1", "silver2", "silver3"]),
+        SectionHeader(section: "Gold", badgeName: ["gold1", "gold2", "gold3"]),
+        SectionHeader(section: "Onyx", badgeName: ["onyx1", "onyx2", "onyx3"]),
+        SectionHeader(section: "Ruby", badgeName: ["ruby1", "ruby2", "ruby3"])
     ]
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,8 +38,9 @@ extension AchievementVC: UICollectionViewDelegate {
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         collectionView.deselectItem(at: indexPath, animated: true)
-        let category = section[indexPath.section]
-        selectedImage = UIImage(named: category.badgeName)
+        let category = section[indexPath.section].badgeName[indexPath.row]
+        selectedImage = UIImage(named: category)
+        print("pressed \(category)")
         
         performSegue(withIdentifier: "achievementDetails", sender: category)
     }
@@ -54,8 +54,8 @@ extension AchievementVC: UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "achievementCell", for: indexPath) as? AchievementCell {
-            let badgeImage = self.section[indexPath.row]
-            cell.badgeImg.image = UIImage(named: "\(badgeImage.badgeName)")
+            let badgeImage = self.section[indexPath.section].badgeName[indexPath.row]
+            cell.badgeImg.image = UIImage(named: "\(badgeImage)")
             return cell
         }
         return AchievementCell()
--- EatingHabits/Presentation/Achievement/SectionHeader.swift
+++ EatingHabits/Presentation/Achievement/SectionHeader.swift
@@ -10,5 +10,5 @@ import Foundation
 
 struct SectionHeader {
     var section: String
-    var badgeName: String
+    var badgeName: [String]
 }
